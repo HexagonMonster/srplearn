4f128529f033f232fd2c4b402738f7ddeeea4805
Hexamon <liluyao0426@126.com>
2024-07-09 01:23:48 +0800

    render

--- Assets/CustomRP/Runtime/CameraRenderer.cs
+++ Assets/CustomRP/Runtime/CameraRenderer.cs
@@ -6,32 +6,78 @@ using UnityEngine.Rendering;
 
 public class CameraRenderer
 {
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
     ScriptableRenderContext context;
     Camera camera;
+    const string bufferName = "Render Camera";
+    CommandBuffer buffer = new CommandBuffer()
+    {
+        name = bufferName,
+    };
+
+    CullingResults cullingResults;
 
     public void Render(ScriptableRenderContext context, Camera camera)
     {
         this.context = context;
         this.camera = camera;
 
+        if (!Cull())
+        {
+            return;
+        }
         Setup();
         DrawVisibleGeometry();
-
         Submit();
+    }
+
+    private bool Cull()
+    {
+        if (camera.TryGetCullingParameters(out var p))
+        {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
     }
 
     private void Setup()
     {
         context.SetupCameraProperties(camera);
+        buffer.ClearRenderTarget(true, true, Color.clear);
+        buffer.BeginSample(bufferName);
+        ExecuteBuffer();
     }
 
     private void DrawVisibleGeometry()
     {
+        var sortingSettings = new SortingSettings(camera)
+        {
+            criteria = SortingCriteria.CommonOpaque,
+        };
+        var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
+        var fileteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref fileteringSettings);
         context.DrawSkybox(camera);
+
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        //drawingSettings.sortingSettings = sortingSettings;
+        fileteringSettings.renderQueueRange = RenderQueueRange.transparent;
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref fileteringSettings);
     }
 
     private void Submit()
     {
+        buffer.EndSample(bufferName);
+        ExecuteBuffer();
         context.Submit();
+    }
+
+    private void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
     }
 }
